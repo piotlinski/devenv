f4b8d6264c850f2ad3961518edef2b863011c516
Piotr Zieliński <piotrekzie100@gmail.com>
2023-09-05 11:23:54 +0200

    Add docker-compose configuration

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 tailscaled &
-tailscale up --authkey=$TS_AUTHKEY --hostname=$TS_HOSTNAME &
+tailscale up --authkey=$TS_AUTHKEY --hostname=$TS_HOSTNAME --accept-routes &
 /usr/sbin/sshd -D -e &
 exec "$@"
